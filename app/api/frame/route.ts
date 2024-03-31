695ab74b31b8b2730148b6c98ee384973736c184
kennyjacobson <kenny.jacobson@gmail.com>
2024-03-30 17:43:10 -0700

    A little clean up.

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -5,36 +5,17 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     const id: any = searchParams.get("id")
     const idAsNumber = parseInt(id)
 
-
-
-    //   const nextId = idAsNumber + 1
-
-    //create a random int  between 1 and 12
     const randomInt = Math.floor(Math.random() * 12) + 1
-    const nextId = randomInt + 1
+    const nextId = idAsNumber + 1
 
-    if (idAsNumber === 700) {
-        return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>This is frame 7</title>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/magic-8-ball/magic-8-ball-11.png" />
-    <meta property="fc:frame:button:1" content="Shake Me!" />
-    <meta property="fc:frame:button:1:action" content="post_redirect" />
-    <meta property="fc:frame:button:2" content="Blog post Tutorial" />
-    <meta property="fc:frame:button:2:action" content="post_redirect" />
-    <meta property="fc:frame:button:3" content="Video Tutorial" />
-    <meta property="fc:frame:button:3:action" content="post_redirect" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
-  </head></html>`);
-    } else {
-        return new NextResponse(`<!DOCTYPE html><html><head>
+    return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/magic-8-ball/magic-8-ball-${randomInt}.png" />
     <meta property="fc:frame:button:1" content="Shake Again" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
   </head></html>`);
-    }
+
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
